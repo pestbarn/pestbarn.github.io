fd898a14501c91d7f148b07f1c54f2e32242bf4f
Mattias Hagberg <pestbarn@gmail.com>
2016-08-21 22:50:41 +0200

    Init Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		jekyll: {
+			options: {
+				bundleExec: true,
+			},
+			dist: {
+				options: {
+					config: '_config.yml'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['assets/scss/*.scss'],
+				tasks: ['compass'],
+			}
+		},
+		compass: {
+			dist: {
+				options: {
+					config: 'config.rb'
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-jekyll');
+	grunt.registerTask('default', ['jekyll','compass']);
+
+	grunt.event.on('watch', function(action, filepath, target) {
+		grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+	});
+}
