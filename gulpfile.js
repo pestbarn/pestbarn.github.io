4a7af51ea20f72a84c80131feb412a18d3ff22f8
Mattias Hagberg <pestbarn@gmail.com>
2016-09-15 21:17:37 +0200

    cssnext deprecated, using postcss-cssnext instead

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssnext = require('cssnext');
+var postcss_cssnext = require('postcss-cssnext');
 var precss = require('precss');
 
 gulp.task('css', function() {
   var processors = [
-    autoprefixer,
-    cssnext,
+    postcss_cssnext,
     precss
   ];
   return gulp.src('./src/css/*.css')
