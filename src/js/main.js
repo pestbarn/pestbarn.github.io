363016b239c27be6edf3a34a3057cfc838391bf5
Mattias Hagberg <pestbarn@gmail.com>
2016-09-20 08:07:55 +0200

    Change to header id

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,10 @@
-function hello() {
-    alert('Hello World!');
-}
-
 var xhr= new XMLHttpRequest();
 xhr.open('GET', 'bin/partials/header.html', true);
 xhr.onreadystatechange= function() {
     if (this.readyState!==4) return;
-    if (this.status!==200) return; // or whatever error handling you want
-    document.getElementsByTagName('header').innerHTML= this.responseText;
+    if (this.status!==200) return;
+    
+document.getElementById('header').innerHTML= 
+this.responseText;
 };
 xhr.send();
