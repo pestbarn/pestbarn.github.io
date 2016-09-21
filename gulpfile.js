04522df17bb8ad8aa66f057330d70f7cddfb1664
Mattias Hagberg <pestbarn@gmail.com>
2016-09-21 17:18:59 +0200

    Rewrite gulpfile.js, add postcss-font-magician and normalize

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,81 @@
-var gulp            = require('gulp');
-var haml            = require('gulp-haml');
-var postcss         = require('gulp-postcss');
-var postcss_cssnext = require('postcss-cssnext');
-var precss          = require('precss');
-var clean_css       = require('gulp-clean-css');
-var concat_css      = require('gulp-concat-css');
-var uglify          = require('gulp-uglify');
-var pump            = require('pump');
+var gulp             = require('gulp');
+var haml             = require('gulp-haml');
+var postcss          = require('gulp-postcss');
+var postcss_cssnext  = require('postcss-cssnext');
+var precss           = require('precss');
+var postcss_magician = require('postcss-font-magician');
+var clean_css        = require('gulp-clean-css');
+var concat_css       = require('gulp-concat-css');
+var uglify           = require('gulp-uglify');
+var pump             = require('pump');
+var vfs              = require('vinyl-fs');
 
 var paths = {
     haml:           './src/haml/index.haml',
     haml_partials:  './src/haml/partials/*.haml',
-    postcss_src:    './src/css/*.css',
-    postcss_bin:    './bin/css/',
-    js_src:         './src/js/*.js',
-    js_bin:         './bin/js/'
+    js:             './src/js/*.js',
+    css: {
+        base: 'src/css/**/*.css',
+        normalize: 'node_modules/normalize.css/normalize.css'
+    }
 };
 
-gulp.task('haml', function(callback) {
+const css = Object.keys(paths.css).map(key => paths.css[key]);
+
+var processors = [
+    postcss_cssnext,
+    precss,
+    postcss_magician
+];
+
+gulp.task('haml', function() {
     pump([
         gulp.src(paths.haml),
         haml({ext: '.html'}),
         gulp.dest('./')
-    ], callback);
+    ]);
 });
 
-gulp.task('haml-partials', function(callback) {
+gulp.task('haml-partials', function() {
     pump([
         gulp.src(paths.haml_partials),
         haml({ext: '.html'}),
         gulp.dest('./bin/partials/')
-    ], callback);
+    ]);
 });
 
 gulp.task('css', function() {
-    var processors = [
-        postcss_cssnext,
-        precss
-    ];
-    return gulp.src(paths.postcss_src)
-    .pipe(postcss(processors))
-    .pipe(concat_css('main.css'))
-    .pipe(clean_css())
-    .pipe(gulp.dest(paths.postcss_bin));
+    pump([
+        vfs.src([css[0],css[1]]),
+        postcss(processors),
+        concat_css('main.css'),
+        vfs.dest('./bin/css/')
+    ]);
 });
 
-gulp.task('js', function(callback){
+gulp.task('minify', ['css'], function(){
     pump([
-        gulp.src(paths.js_src),
+        vfs.src([css[0],css[1]]),
+        postcss(processors),
+        concat_css('main.min.css'),
+        clean_css(),
+        vfs.dest('./bin/css/')
+    ]);
+})
+
+gulp.task('js', function(){
+    pump([
+        gulp.src(paths.js),
         uglify(),
-        gulp.dest(paths.js_bin),
-    ], callback
-);
+        gulp.dest('./bin/js/'),
+    ]);
 });
 
 gulp.task('watch', function() {
     gulp.watch(paths.haml, ['haml']);
     gulp.watch(paths.haml_partials, ['haml-partials']);
-    gulp.watch(paths.postcss_src, ['css']);
-    gulp.watch(paths.js_src, ['js']);
+    gulp.watch(paths.css.base, [['css'],['minify']]);
+    gulp.watch(paths.js, ['js']);
 });
 
-gulp.task('default', ['haml', 'haml-partials', 'css', 'js']);
+gulp.task('default', ['haml', 'haml-partials', 'css', 'minify', 'js']);
