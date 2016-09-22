dc0f27bc27d8232272a56bb7842c6843ed215b74
Mattias Hagberg <pestbarn@gmail.com>
2016-09-22 17:13:16 +0200

    Begin parsing json

--- src/js/main.js
+++ src/js/main.js
@@ -35,7 +35,7 @@ HeaderModule = {
             }
         };
         xhr.ontimeout = function () {
-            alert("The request for " + url + " timed out.");
+            alert('The request for ' + url + ' timed out.');
         };
         xhr.timeout = 1000;
         xhr.send();
@@ -43,6 +43,38 @@ HeaderModule = {
 
 };
 
+var params,
+SkillsModule = {
+
+    fetchJSON: function(file, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.overrideMimeType('application/json');
+        xhr.open('GET', file, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    callback(xhr.responseText);
+                }
+            }
+        };
+        xhr.send();
+    },
+
+    loadJSON: function(file) {
+        this.fetchJSON(file, function(response) {
+            var fetchedJSON = JSON.parse(response);
+            console.log(fetchedJSON);
+        });
+    },
+
+    init: function() {
+        this.loadJSON('experience.json');
+    }
+
+};
+
+
 document.addEventListener('DOMContentLoaded', function() {
     HeaderModule.init();
+    SkillsModule.init();
 });
