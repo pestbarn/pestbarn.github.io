a6dd681800cd0aa1f5303d77f2e67812bdab2ddd
Mattias Hagberg <pestbarn@gmail.com>
2016-12-15 11:38:33 +0100

    Untappd added

--- src/js/main.js
+++ src/js/main.js
@@ -82,12 +82,12 @@ var Parser = (function() {
                     var title = cells.feed.entry;
                     var updated = cells.feed.updated.$t;
                     updated = `Last updated: ${updated.substr(0,10)}`;
-                    é('.mdl-spinner').remove();
-                    é('#scroll-tab-1 .mdl-tooltip').innerHTML = updated;
+                    é('#attended-gigs .mdl-spinner').remove();
+                    é('#gigs .mdl-tooltip').innerHTML = updated;
                     var sort = document.getElementById('giglist');
 
                     for (let t in title) {
-                        if (t > 0) Gigs.gigList(title[t].gsx$headline.$t,
+                        if (t > 0) Tables.gigList(title[t].gsx$headline.$t,
                                                 title[t].gsx$text.$t,
                                                 title[t].gsx$startdate.$t);
                         if (t == title.length-1){
@@ -96,6 +96,7 @@ var Parser = (function() {
                                 valueNames: ['gig-name', 'gig-location', 'gig-date']
                             };
                             var userList = new List('attended-gigs', options);
+                            userList;
                         }
                     }
                 },
@@ -104,6 +105,46 @@ var Parser = (function() {
                 }
             };
             $http(gigs).get()
+            .then(callback.success)
+            .catch(callback.error);
+        },
+        fetchBeer: function() {
+            let beer = '//spreadsheets.google.com/feeds/list/1a056ruITWMr8oeJECb8QM6ePe00IqTTEIkrkhY-QeMI/1/public/values?alt=json';
+
+            var callback = {
+                success: function(data) {
+                    var cells = JSON.parse(data);
+                    var title = cells.feed.entry;
+                    var updated = cells.feed.updated.$t;
+                    updated = `Last updated: ${updated.substr(0,10)}`;
+                    é('#untappd-stats .mdl-spinner').remove();
+                    é('#beers .mdl-tooltip').innerHTML = updated;
+                    var sort = document.getElementById('beerlist');
+
+                    for (let t in title) {
+                        var dateTrim = title[t].gsx$createdat.$t;
+                        dateTrim = dateTrim.substr(0,10);
+                        if (t > 0) Tables.beerList(title[t].gsx$breweryname.$t,
+                                                title[t].gsx$beername.$t,
+                                                title[t].gsx$beertype.$t,
+                                                title[t].gsx$beerabv.$t,
+                                                title[t].gsx$ratingscore.$t,
+                                                dateTrim);
+                        if (t == title.length-1){
+                            sorttable.makeSortable(sort);
+                            var options = {
+                                valueNames: ['brewery', 'beer-name', 'beer-type', 'beer-abv', 'beer-rating', 'beer-date']
+                            };
+                            var userList = new List('untappd-stats', options);
+                            userList;
+                        }
+                    }
+                },
+                error: function(data) {
+                    throw new Error(data);
+                }
+            };
+            $http(beer).get()
             .then(callback.success)
             .catch(callback.error);
         }
@@ -194,7 +235,7 @@ var Render = {
 
     init: function() {
         if (Request.isIndex()) Parser.fetchContent();
-        if (Request.isPage('/gigs/')) Parser.fetchGigs();
+        if (Request.isPage('/stats/')) Parser.fetchGigs(); Parser.fetchBeer();
     }
 
 };
@@ -246,7 +287,7 @@ var Request = {
 
 };
 
-var Gigs = {
+var Tables = {
     // NOT USED ON MAIN PAGE
 
     gigList: function(name, location, date) {
@@ -262,6 +303,29 @@ var Gigs = {
 
         var tr = ç('tr');
         tr.innerHTML = gig;
+        frag.appendChild(tr);
+        list.appendChild(frag);
+    },
+
+    beerList: function(brewery, bName, bType, bAbv, bRating, bDate) {
+        var frag = document.createDocumentFragment();
+        var list = é('#beerlist tbody');
+        //['brewery', 'beer-name', 'beer-type', 'beer-abv', 'beer-rating', 'beer-date']
+        var brew = `<td class="mdl-data-table__cell--non-numeric brewery">
+                ${brewery}
+            </td>
+            <td class="mdl-data-table__cell--non-numeric beer-name">
+                ${bName}
+            </td>
+            <td class="mdl-data-table__cell--non-numeric beer-type">
+                ${bType}
+            </td>
+            <td class="beer-abv">${bAbv}</td>
+            <td class="beer-rating">${bRating}</td>
+            <td class="beer-date">${bDate}</td>`;
+
+        var tr = ç('tr');
+        tr.innerHTML = brew;
         frag.appendChild(tr);
         list.appendChild(frag);
     }
