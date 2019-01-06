acbd27757d92ad313639cac62117f10315a52524
Mattias Hagberg <hello@mattias.pw>
2019-01-06 17:48:38 +0100

    Minor linting

--- src/scripts/experience.test.js
+++ src/scripts/experience.test.js
@@ -1,12 +1,10 @@
-const globals = require('./globals');
-const setObj = globals.setObj;
-const getObj = globals.getObj;
 const axios = require('axios');
 
 jest.mock('axios');
 
 test('Experience json is fetched', () => {
     const experienceJson = require('../experience.json');
+    // eslint-disable-next-line
     const resp = { items: [{ "dateFrom": "2017/10", "dateTo": "(current)", "position": "Frontend developer", "title": "iStone", "url": "istone.com" }, { "dateFrom": "2017/03", "dateTo": "2017/10", "position": "Frontend developer", "title": "Magic by iStone", "url": "magicbyistone.se" }, { "dateFrom": "2015/06", "dateTo": "2017/03", "position": "Frontend developer", "title": "Sportamore", "url": "sportamore.se" }, { "dateFrom": "2012/09", "dateTo": "2016/05", "position": "Festival organizer, developer, designer", "title": "I:O:A" }, { "dateFrom": "2011/05", "dateTo": "2011/09", "position": "Frontend developer & designer", "title": "46elks", "url": "46elks.com" }, { "dateFrom": "2011/01", "dateTo": "2015/05", "position": "Frontend developer & designer", "title": "Freespee", "url": "freespee.com" }, { "dateFrom": "2006/05", "dateTo": "2012/01", "position": "Webmaster, developer, writer", "title": "SwedenMetal" }] };
 
     axios.get.mockResolvedValue(resp);
--- src/scripts/globals.js
+++ src/scripts/globals.js
@@ -3,7 +3,7 @@ exports.setObj = function setObj(key, value) {
         this.setItem(key, JSON.stringify(value));
     };
     return localStorage.setObject(key, value);
-}
+};
 
 exports.getObj = function getObj(key) {
     Storage.prototype.getObject = function (key) {
@@ -11,4 +11,4 @@ exports.getObj = function getObj(key) {
         return value && JSON.parse(value);
     };
     return localStorage.getObject(key);
-}
+};
