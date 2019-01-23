3608f292583f90e422e9a69b6af5c01183da3aeb
Mattias Hagberg <hello@mattias.pw>
2019-01-23 04:03:23 +0100

    Update API calls

--- src/scripts/profile.test.js
+++ src/scripts/profile.test.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-// const logo = require('./profile');
 
 jest.mock('axios');
 
--- src/scripts/social.js
+++ src/scripts/social.js
@@ -1,19 +1,20 @@
 const axios = require('axios');
+const env = require('../../.env.js');
+const untappd_clientID = env.config.UNTAPPD_CLIENTID;
+const untappd_clientSecret = env.config.UNTAPPD_CLIENTSECRET;
+const lastfm_key = env.config.LASTFM_KEY;
 
 const renderUntappd = () => {
     axios
-        .get('https://dl.dropboxusercontent.com/s/p5c05yrjhmpqel6/Untappd.json')
+        .get(`https://api.untappd.com/v4/user/beers/pestbarn?client_id=${untappd_clientID}&client_secret=${untappd_clientSecret}`)
         .then(result => {
-            const beers = result.data;
-            const beer = beers.slice(-1)[0];
-
-            getBeer(beer);
+            const beers = result.data.response.beers.items[0];
+            getBeer(beers);
         });
 
     function getBeer(beer) {
-        const brewery = beer.brewery_name,
-            beerName = beer.beer_name,
-            url = beer.checkin_url;
+        const brewery = beer.brewery.brewery_name,
+            beerName = beer.beer.beer_name;
 
         const elements = ['#brewery', '#beer'];
 
@@ -21,13 +22,13 @@ const renderUntappd = () => {
             document.querySelector(el).innerText = i === 0 ? brewery : beerName;
         });
 
-        document.getElementById('untappd-url').setAttribute('href', url);
+        document.getElementById('untappd-url').setAttribute('href', 'https://untappd.com/user/pestbarn');
     }
 };
 
 const renderMusic = () => {
     axios
-        .get('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=pestbarn&api_key=9ab0d2d4d5ec7e287ea35022d52d09e4&limit=2&format=json')
+        .get(`https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=pestbarn&api_key=${lastfm_key}&limit=2&format=json`)
         .then(result => {
             const tracks = result.data.recenttracks.track;
             const track = tracks.slice(-1)[0];
