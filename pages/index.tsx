ff3d77a59987cd2aa2e3c35a5d3ec1f7b626663f
Mattias Hagberg <hello@mattias.pw>
2020-10-12 18:17:40 +0200

    Init Next.js with Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import { GetStaticProps } from 'next'
+
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
+
+/* export const getStaticProps: GetStaticProps = async () => {
+  return {}
+} */
