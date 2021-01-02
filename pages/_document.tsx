3a62b42eb39ec3004e32bc6f8c65e2ff7f45a752
Mattias Hagberg <pestbarn@gmail.com>
2021-01-02 17:37:51 +0100

    WIP: adding Flat Surface Shader

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,34 @@ export default class MyDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
+                    <div id="fss"></div>
+
+                    <script src="fss.min.js"></script>
+                    <script>
+                        // 1) Create a Renderer for the context you would like to render to.
+                        //    You can use either the WebGLRenderer, CanvasRenderer or SVGRenderer.
+                        var renderer = new FSS.SVGRenderer();
+
+                        // 2) Add the Renderer's element to the DOM:
+                        var container = document.getElementById('fss');
+                        container.appendChild(renderer.element);
+
+                        // 3) Create a Scene:
+                        var scene = new FSS.Scene();
+
+                        // 4) Create some Geometry & a Material, pass them to a Mesh constructor, and add the Mesh to the Scene:
+                        var geometry = new FSS.Plane(200, 100, 4, 2);
+                        var material = new FSS.Material('#444444', '#FFFFFF');
+                        var mesh = new FSS.Mesh(geometry, material);
+                        scene.add(mesh);
+
+                        // 5) Create and add a Light to the Scene:
+                        var light = new FSS.Light('#FF0000', '#0000FF');
+                        scene.add(light);
+
+                        // 6) Finally, render the Scene:
+                        renderer.render(scene);
+                    </script>
                 </body>
             </Html>
         );
